3ff2aabe0449595da9ad3d73ceaa20befadf2ed2
Kirill <dukennt@gmail.com>
2018-08-21 20:36:19 +0000

    dinamic routes

--- headlines.py
+++ headlines.py
@@ -11,35 +11,33 @@ app = Flask(__name__)
 BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
 LENTA_TOP_FEED = "https://lenta.ru/rss/top7"
 
+RSS_FEEDS = {
+    'bbs': 'http://feeds.bbci.co.uk/news/rss.xml',
+    'cnn': 'http://rss.cnn.com/rss/edition.rss',
+    'fox': 'http://feeds.foxnews.com/foxnews/latest',
+    'iol': 'http://www.iol.co.za/cmlink/1.640',
+    'lenta' : "https://lenta.ru/rss/top7"
+}
+
 
 
 @app.route("/")
-def get_news():
-    feed = feedparser.parse(BBC_FEED)
+@app.route("/<publication>")
+def get_news(publication='lenta'):
+    feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
     return '''<html>
             <body>
-                <h1> BBC Headlines </h1>
+                <h1>Headlines</h1>
                 <b>{0}</b></br>
                 <i>{1}</i></br>
                 <p>{2}</p></br>
             </body>
-            </html>'''.format(first_article.get("title"),first_article.get("published"),
-                                first_article.get("summary"))
-
-# route for lenta.ru
-@app.route("/lenta-top7")
-def get_news_lenta():
-    feed = feedparser.parse(LENTA_TOP_FEED)
-    first_article = feed['entries'][0]
-    return '''<html>
-            <body>
-                <h1> Lenta.ru Headlines </h1>
-                <b>{0}</b></br>
-                <i>{1}</i></b>
-            </body>
-            </html>'''.format(first_article.get("title"), first_article.get("description"))
-
+            </html>
+    '''.format(first_article.get("title"), 
+               first_article.get("published"),    
+               first_article.get("summary"),    
+    )
     
 
 if __name__ == '__main__':
